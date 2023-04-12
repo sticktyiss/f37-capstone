7e12e46ae990764519c92b0cfc8d112448f0372e
Tyiss Goodman <sticktyiss@gmail.com>
2023-04-12 12:17:49 -0700

    Front end displays backend in gallery

--- public/main.js
+++ public/main.js
@@ -1,6 +1,8 @@
 const paintingsBtn = document.querySelector('#paintings')
 const resinBtn = document.querySelector('#resin')
 const jewleryBtn = document.querySelector('#jewelry')
+const allBtn = document.querySelector('#all')
+const gallery = document.querySelector('#gallery')
 
 const baseURL = "http://localhost:4754"
 
@@ -8,6 +10,7 @@ const printPaintings = () => {
   axios.get(`${baseURL}/paintings`)
   .then((res) => {
     console.log(res.data)
+    printCard(res.data)
   })
   .catch((theseHands) => console.log(theseHands))
 }
@@ -15,6 +18,7 @@ const printJewelry = () => {
   axios.get(`${baseURL}/jewelry`)
   .then((res) => {
     console.log(res.data)
+    printCard(res.data)
   })
   .catch((theseHands) => console.log(theseHands))
 }
@@ -22,16 +26,39 @@ const printResin = () => {
   axios.get(`${baseURL}/resin`)
   .then((res) => {
     console.log(res.data)
+    printCard(res.data)
   })
   .catch((theseHands) => console.log(theseHands))
 }
 const printAll = () => {
-  printResin()
-  printJewelry()
   printPaintings()
+  printJewelry()
+  printResin()
+}
+//function to print card
+const printCard = item => {
+  for(let i=0; i<item.length; i++){
+    let card = document.createElement('div')
+    card.innerHTML = `
+    <img src='${item[i].image}'>
+    <h3>${item[i].title}</h3>
+    <p>${item[i].desc}</p>
+    <p class='prices'>${item[i].price}</p>
+    `
+    gallery.appendChild(card)
+  }
 }
 
-// printAll() It works great
-paintingsBtn.addEventListener('click', printPaintings)
-jewleryBtn.addEventListener('click', printJewelry)
-resinBtn.addEventListener('click', printResin)
+printAll()
+allBtn.addEventListener('click', () => {
+  gallery.innerHTML = ''
+  printAll()})
+paintingsBtn.addEventListener('click', () => {
+  gallery.innerHTML = ''
+  printPaintings()})
+jewleryBtn.addEventListener('click', () => {
+  gallery.innerHTML = ''
+  printJewelry()})
+resinBtn.addEventListener('click', () => {
+  gallery.innerHTML = ''
+  printResin()})
